6e785bfb445f20ea11d9575d118ad0da5b1a4635
Hrithik Shet <hrithik356hrithik@gmail.com>
2024-05-26 17:22:51 +0530

    latest change

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -1,101 +1,31 @@
-// import React, { useEffect, useState } from 'react';
-// import '../index.css';
-// import { Link, useNavigate } from 'react-router-dom';
-// import axios from 'axios';
-// import { Navigate } from 'react-router-dom';
-// import { Button } from '@mui/material';
-// import Result from './ResultPage';
-// import { alignProperty } from '@mui/material/styles/cssUtils';
-// import { AlignHorizontalCenter } from '@mui/icons-material';
-
-
-
-
-// const Lists = () => {
-//     const navigate= useNavigate();
-//   const [lists, setLists] = useState([]);
-//   const [loading, setLoading] = useState(true);
-//   const [error, setError] = useState(null);
-//   const BEARER_TOKEN = localStorage.getItem('accessToken')
-
-//   const Listhandle = (list1) =>{
-//     console.log('listhandle')
-//     const navigate=useNavigate();
-//       return()=>{
-//         // navigate(`/Results`)
-//         <Result list_name ={list1}/>
-//       }
-//   }
-
-//   useEffect(() => {
-//     axios.get('http://127.0.0.1:8000/list/',{
-//      headers: {
-//            'Authorization': `Bearer ${BEARER_TOKEN}`
-//          }
-//     })
-//       .then(response => response.data)
-//       .then(data => {
-//          const sortedListNames = data.sort((a, b) => a.localeCompare(b));
-//         setLists(sortedListNames);
-//         setLoading(false);
-//       })
-//       .catch(error => {
-//         setError(error);
-//         setLoading(false);
-//       });
-//   }, []);
-
-//   if (loading) return <div>Loading...</div>;
-//   if (error) return <div>Error: {error.message}</div>;
-//   if(!localStorage.getItem('accessToken')) 
-//    navigate('/login')
-
-//   return (
-//     <div className='list' style={{color: "#252525" ,  letterSpacing: 'normal'}}>
-//       <h1></h1><br/>
-//       <ListContainer lists={lists} />
-//     </div>
-//   );
-// };
-
-// const ListContainer = ({ lists }) => {
-//   return (
-//     <div className='list-Container'>
-//       {lists.map((list, index) => (
-//         <List key={index} name={list} />
-//       ))}
-//     </div>
-//   );
-// };
-
-// const List = ({ name }) => {
-//   return (
-//     <div className="list-item">
-//       <Link to={`/Results/${name}`}>
-//       <h2>{name}</h2>
-//     </Link>
-//     </div>
-//   );
-// };
-
-// export default Lists;
-
-
 import React, { useEffect, useState } from 'react';
 import '../index.css';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import Result from './ResultPage';
+import { Navigate } from 'react-router-dom';
 import { Button } from '@mui/material';
+import Result from './ResultPage';
+import { alignProperty } from '@mui/material/styles/cssUtils';
+import { AlignHorizontalCenter } from '@mui/icons-material';
 
 const CACHE_KEY = 'listsCache';
 const CACHE_EXPIRY = 1000 * 60 * 60;
+
 const Lists = () => {
-  const navigate = useNavigate();
+    const navigate= useNavigate();
   const [lists, setLists] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const BEARER_TOKEN = localStorage.getItem('accessToken');
+  const BEARER_TOKEN = localStorage.getItem('accessToken')
+
+  const Listhandle = (list1) =>{
+    console.log('listhandle')
+    const navigate=useNavigate();
+      return()=>{
+        // navigate(`/Results`)
+        <Result list_name ={list1}/>
+      }
+  }
 
   useEffect(() => {
     const cachedData = localStorage.getItem(CACHE_KEY);
@@ -107,15 +37,14 @@ const Lists = () => {
         return;
       }
     }
-
-    axios.get('http://127.0.0.1:8000/list/', {
-      headers: {
-        'Authorization': `Bearer ${BEARER_TOKEN}`
-      }
+    axios.get('http://127.0.0.1:8000/list/',{
+     headers: {
+           'Authorization': `Bearer ${BEARER_TOKEN}`
+         }
     })
       .then(response => response.data)
       .then(data => {
-        const sortedListNames = data.sort((a, b) => a.localeCompare(b));
+         const sortedListNames = data.sort((a, b) => a.localeCompare(b));
         setLists(sortedListNames);
         setLoading(false);
         localStorage.setItem(CACHE_KEY, JSON.stringify({ data: sortedListNames, timestamp: Date.now() }));
@@ -124,18 +53,20 @@ const Lists = () => {
         setError(error);
         setLoading(false);
       });
-  }, [BEARER_TOKEN]);
+  }, []);
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
-  if (!BEARER_TOKEN) {
-    navigate('/login');
-    return null;
-  }
+  if(!localStorage.getItem('accessToken'))
+    {
+      navigate('/login');
+      return null;
+    }
+  
 
   return (
-    <div className='list' style={{ color: "#252525", letterSpacing: 'normal' }}>
-      <h1></h1><br />
+    <div className='list' style={{color: "#252525" ,  letterSpacing: 'normal'}}>
+      <h1></h1><br/>
       <ListContainer lists={lists} />
     </div>
   );
@@ -155,8 +86,8 @@ const List = ({ name }) => {
   return (
     <div className="list-item">
       <Link to={`/Results/${name}`}>
-        <h2>{name}</h2>
-      </Link>
+      <h2>{name}</h2>
+    </Link>
     </div>
   );
 };
--- src/pages/ResultPage.js
+++ src/pages/ResultPage.js
@@ -1,74 +1,24 @@
-// import React, { useEffect, useState } from 'react';
-// import ResumeList from './ResumeList';
-// import '../index.css';
-// import {  useNavigate } from 'react-router-dom';
-// import axios from 'axios';
-// import { useParams } from 'react-router-dom';
-
-
-// const Result = () => {
-//   const { list_name } = useParams();
-//   const navigate= useNavigate();
-//   const [data, setData] = useState([]);
-//   const [loading, setLoading] = useState(true);
-//   const [error, setError] = useState(null);
-//   const BEARER_TOKEN = localStorage.getItem('accessToken')
-
-//   useEffect(() => {
-//      axios.get(`http://127.0.0.1:8000/list/${list_name}/rankresumes`,{
-//       headers: {
-//             'Authorization': `Bearer ${BEARER_TOKEN}`
-//           }
-//      })
-//       .then(response => response.data)
-//       .then(data => {
-//         const sortedData = data.sort((a, b) => b.score - a.score);
-//         setData(sortedData);
-//         setLoading(false);
-//       })
-//       .catch(error => {
-//         setLoading(false);
-//       });
-//   },[]);
-
-
-//   if (loading) return (
-//   <div style={{color: "#252525", textAlign: 'center'}}>Loading...</div>
-// );
-//   if (error) return <div>Error: {error.message}</div>;
-//   if(!localStorage.getItem('accessToken')) 
-//    navigate('/')
-//   return (
-//     <div className="App" style={{color: "#252525" ,  letterSpacing: 'normal'}}>
-//       <h1 style={{color: '#7f56da' }}>Resume List</h1>
-//       <ResumeList data={data} />
-//     </div>
-    
-//   );
-// };
-
-// export default Result;
-
 import React, { useEffect, useState } from 'react';
 import ResumeList from './ResumeList';
 import '../index.css';
-import { useNavigate, useParams } from 'react-router-dom';
+import {  useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
 const CACHE_KEY_PREFIX = 'resultCache_';
 const CACHE_EXPIRY = 1000 * 60 * 60;
 
 const Result = () => {
   const { list_name } = useParams();
-  const navigate = useNavigate();
+  const navigate= useNavigate();
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const BEARER_TOKEN = localStorage.getItem('accessToken');
-  const cacheKey = `${CACHE_KEY_PREFIX}${list_name}`;
+  const BEARER_TOKEN = localStorage.getItem('accessToken')
+const cacheKey = `${CACHE_KEY_PREFIX}${list_name}`;
 
   useEffect(() => {
-    const cachedData = localStorage.getItem(cacheKey);
+  const cachedData = localStorage.getItem(cacheKey);
     if (cachedData) {
       const { data, timestamp } = JSON.parse(cachedData);
       if (Date.now() - timestamp < CACHE_EXPIRY) {
@@ -77,42 +27,41 @@ const Result = () => {
         return;
       }
     }
-
-    axios.get(`http://127.0.0.1:8000/list/${list_name}/rankresumes`, {
+     axios.get(`http://127.0.0.1:8000/list/${list_name}/rankresumes`,{
       headers: {
-        'Authorization': `Bearer ${BEARER_TOKEN}`
-      }
-    })
+            'Authorization': `Bearer ${BEARER_TOKEN}`
+          }
+     })
       .then(response => response.data)
       .then(data => {
         const sortedData = data.sort((a, b) => b.score - a.score);
         setData(sortedData);
         setLoading(false);
-        localStorage.setItem(cacheKey, JSON.stringify({ data: sortedData, timestamp: Date.now() }));
+localStorage.setItem(cacheKey, JSON.stringify({ data: sortedData, timestamp: Date.now() }));
       })
       .catch(error => {
-        setError(error);
         setLoading(false);
       });
-  }, [BEARER_TOKEN, cacheKey, list_name]);
+  },[]);
+
 
   if (loading) return (
-    <div style={{ color: "#252525", textAlign: 'center' }}>Loading...</div>
-  );
+  <div style={{color: "#252525", textAlign: 'center'}}>Loading...</div>
+);
   if (error) return <div>Error: {error.message}</div>;
-  if (!BEARER_TOKEN) {
-    navigate('/');
-    return null;
-  }
-
+  if(!localStorage.getItem('accessToken')) {
+  navigate('/');
+  return null;
+}
+   
   return (
-    <div className="App" style={{ color: "#252525", letterSpacing: 'normal' }}>
-      <h1 style={{ color: '#7f56da' }}>Resume List</h1>
+    <div className="App" style={{color: "#252525" ,  letterSpacing: 'normal'}}>
+      <h1 style={{color: '#7f56da' }}>Resume List</h1>
       <ResumeList data={data} />
     </div>
+    
   );
 };
 
 export default Result;
 
-  
